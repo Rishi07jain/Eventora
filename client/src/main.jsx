774b95360c3c3b76982002968fe99f988248e456
Rishi Jain <rishijain70777@gmail.com>
2026-07-15 23:42:48 +0530

    Fixed tailwind issue and Admin dash export

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 
@@ -10,4 +11,3 @@ createRoot(document.getElementById('root')).render(
     </AuthProvider>
   </StrictMode>,
 )
- 
